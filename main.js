0431b0844bd01dd364b8ec00f4ae4e6aca0a7a55
ZenArcade17 <59713429+ZenArcade17@users.noreply.github.com>
2020-03-06 11:03:41 -0500

    ready to read anchor fragments

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
+let homeMap = L.map('map').setView([44.475, -73.212], 15);
+
+L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
+}).addTo(homeMap);
 
 fetch('https://json-server.burlingtoncodeacademy.now.sh/restaurants')
